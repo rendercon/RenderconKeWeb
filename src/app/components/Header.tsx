dcb350c4e03a1d1d704614e0dc0b2685071e327a
rdke254 <133155462+rdke254@users.noreply.github.com>
2023-06-01 14:45:17 +0300

    Update Header.tsx
    event dates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-gray-50 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <p>
-              <time dateTime="2023-09-30">September 2023</time>
+              <time dateTime="2023-09-30">September 29 -30 2023</time>
             </p>
             <p>•</p>
             <p>Nairobi, Kenya</p>
