c03c11eae0d10c1761e62c0a2f227cef97c77b8e
orama254 <orama209@gmail.com>
2023-06-15 19:19:42 +0300

    changes to the landing page

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -14,15 +14,23 @@ type Partners = {
 
 
 
-const partners: Partners[] = [
+const partners: any = [
     // partners name and logo goes here
     {
-        name: 'ReactAdvancedLondon',
-        logo: LogoReactAdvancedLondon,
+        // name: 'ReactAdvancedLondon',
+        // logo: LogoReactAdvancedLondon,
     },
   ]
 
 const Partners = () => {
+
+    const addr = "partnerships";
+    const tld = "rendercon.org";
+
+    const handleEmailClick = (e : React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+        window.location.href = `mailto:${addr}@${tld}`;
+    }
 
 
   return (
@@ -31,8 +39,13 @@ const Partners = () => {
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-slate-200 sm:text-5xl">
                 Our Partners
         </h2>
-        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
-          {partners.map((partner) => (
+        <p className="text-slate-400 text-xl mt-20 font-mono">
+            We are always looking for partners to help us create an amazaing conference experience for our attendees. <br/> 
+            If you are interested in partnering with us, please reach out to us <a href="#" onClick={handleEmailClick}><u>HERE</u></a>.
+        </p>
+            
+        {/* <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
+          { partners.map((partner: any) => (
             <div
               key={partner.name}
               className="flex items-center justify-center"
@@ -40,7 +53,7 @@ const Partners = () => {
               <Image src={partner.logo} alt={partner.name} unoptimized height={150} />
             </div>
           ))}
-        </div>
+        </div> */}
       </Container>
     </section>
   )
