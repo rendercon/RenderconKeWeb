5e1ceca53d81a5aa1bc88df715757b550daf2bce
orama254 <orama209@gmail.com>
2024-03-25 16:34:02 +0300

    feat:Add details for rendercon 2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,13 @@ const kronaOne = Krona_One({
 })
 
 export const metadata = {
-  title: 'RenderconKe 2023 — The premier conference experience for Web and Mobile',
-  description: 'RenderconKe 2023 is a conference for Web and Mobile developers. We bring together a diverse group of people from all across the world to share their knowledge and experience.',
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://renderconke.org',
+    site_name: 'Rendercon Kenya',
+    title: 'RenderconKe 2024 — The premier conference experience for Web and Mobile',
+    description: 'RenderconKe 2024 is a conference for Web and Mobile developers. We bring together a diverse group of people from all across the world to share their knowledge and experience.',
+    image: 'https://res.cloudinary.com/djgfcdjgn/image/upload/v1711373281/rendercon_24_sessionize_banner_s00r58.png',    
 }
 
 export default function RootLayout({
