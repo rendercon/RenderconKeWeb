66b3d1e0a6b97117919d3fff7d50b238f09b45b0
wangui-wamutitu <99166306+wangui-wamutitu@users.noreply.github.com>
2024-08-22 17:04:49 +0300

    chore/adds-navbar (#26)
    * chore/adds-navbar
    
    * chore/added-responsiveness-for-small-screens
    
    * fix/props-on-LargeScreenNavbar-component
    
    * Add the navigation and make it responsive on all screens
    
    * Add the navigation and make it responsive on all screens
    
    ---------
    
    Co-authored-by: kandiecindy <kandie.cindy@globallinkplus.com>

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,23 +1,54 @@
+'use client'
+
 import Link from 'next/link'
 import { Button } from './Button'
 import { Container } from './Container'
+import Navbar from './Navbar'
+import renderconWB from '@/app/images/logos/Rendercon-wb.png'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { RiMenu3Line } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
 
-export function Header() {
-  return (
-    <header className="relative z-50 pb-11 lg:pt-8 md:sticky top-0 bg-[#42208c] bg-opacity-80">
-      <Container className="flex flex-wrap items-center justify-between align-center sm:justify-between lg:flex-nowrap">
-      <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
-          <span className='text-slate-200 text-2xl font-semibold'>RenderConKe</span>
-        </div>
-        <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-100/10 py-4 font-mono text-sm text-gray-50 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
-          <div className="mx-auto flex items-center gap-4 px-4">
-            <p>
-              <time dateTime="2024-10-04">04</time> - 
-              <time dateTime="2024-10-05"> 05 of October, 2024</time>
-            </p>
-            <p>•</p>
-            <p>Nairobi, Kenya</p>
+type TNavbar={
+  scrolled:boolean, 
+}
+
+function MobileNavbar(props: TNavbar){
+  const {scrolled} = props
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false)
+
+  return(
+    <header className={`z-50 w-full sticky top-0 px-4 ${scrolled? 'bg-[#240046] bg-opacity-100' : "bg-[#42208c] bg-opacity-80"}`}>
+      <div className={'w-full h-[80px] flex justify-between items-center'}>
+        <Link href="#home"><Image src={renderconWB} alt={'Rendercon white background logo'} unoptimized height={50}/></Link>
+        <button onClick={() => setIsMobileMenuOpened(!isMobileMenuOpened)}>{isMobileMenuOpened ? <RxCross2  className={'text-white text-2xl'}/> : <RiMenu3Line className={'text-white text-2xl'}/>}</button>
+      </div>
+      {isMobileMenuOpened ? 
+        <div className={'flex flex-col py-4 pl-2'}>
+          <div className={'font-mono text-gray-50 pb-4'}>
+            <Navbar/>
           </div>
+          <Button>
+            <Link href="https://paydexp.com/renderconke-2024" target="_blank" rel="noopener noreferrer">Get Ticket</Link>
+          </Button>
+        </div>
+      : null}
+    </header>
+  )
+}
+
+function LargeScreenNavbar(props: TNavbar){
+  const {scrolled} = props
+
+  return(
+    <header className={`relative z-50 pb-11 lg:pt-8 md:sticky top-0 ${scrolled? 'bg-[#240046] bg-opacity-100 border-b-2' : "bg-[#42208c] bg-opacity-80"}`} >
+      <Container className="flex flex-wrap items-center justify-between align-center sm:justify-between lg:flex-nowrap">
+        <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
+          <Link href="#home"><Image src={renderconWB} alt={'Rendercon white background logo'} unoptimized height={60}/></Link>
+        </div>
+        <div className={'flex flex-col font-mono text-gray-50 items-center justify-center'}>
+          <Navbar/>
         </div>
         <div className="mt-5 pl-10  sm:mt-10 sm:flex md:pl-0 lg:pl-0 lg:mt-0 lg:grow lg:basis-0 lg:justify-end">
           <Button>
@@ -27,5 +58,40 @@ export function Header() {
         </div>
       </Container>
     </header>
+  )
+
+}
+
+
+export function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(() => {
+    const checkScroll = () => {
+      setScrolled(window.scrollY > 200); 
+    };
+    
+    window.addEventListener('scroll', checkScroll);
+    return () => window.removeEventListener('scroll', checkScroll);
+  }, []);
+
+  useEffect(() => {
+    function checkInnerWidth(){
+      const windowInnerWidth = window.innerWidth
+      if (windowInnerWidth <= 1023) {
+        setShowMobileMenu(true);
+      }else{
+        setShowMobileMenu(false)
+      }
+    }
+
+    window.addEventListener('resize', checkInnerWidth)
+    return () => window.removeEventListener('resize', checkInnerWidth)
+  }, [])
+
+
+  return (
+    showMobileMenu ? <MobileNavbar scrolled={scrolled}/> : <LargeScreenNavbar scrolled={scrolled}/>
   )
 }
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <ul className="w-full lg:w-[600px] flex flex-col h-[120px] lg:h-auto lg:flex-row font-mono justify-between lg:items-center">
+        {/* <li><Link href="#">Agenda</Link></li> */}
+        <li><Link href="#speakers">Speakers</Link></li>
+        <li><Link href="#sponsors">Sponsors</Link></li>
+        <li><Link href="#contacts">Contacts</Link></li>
+    </ul>
+  )
+}
+
+export default Navbar
