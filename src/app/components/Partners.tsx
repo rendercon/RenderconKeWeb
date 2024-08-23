b9f1d8faf2a6df4d32923f1e20bf3109362b2c5d
Cindy Kandie <37507239+cindykandie@users.noreply.github.com>
2024-08-23 22:43:11 +0300

    Ui changes on the general ui especially sponsors and partners (#30)
    Co-authored-by: kandiecindy <kandie.cindy@globallinkplus.com>

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -1,90 +1,86 @@
 'use client';
 
-import Image from 'next/image'
-
+import Image, { StaticImageData } from 'next/image';
 import { Container } from './Container';
 
-// import LogoReactAdvancedLondon from '../images/logos/ReactAdvancedLondon.png'
 import LogoDevKenya from '../images/logos/devkenya_logo.png';
 import LogoReactDevsKe from '../images/logos/ReactdevskeLogo.png';
 import LogoOscaNairobi from '../images/logos/osca_nairobi_logo.jpg';
 import LogoSpaceYaTech from '../images/logos/SPACEYATECH_LOGO.jpg';
 import LogoSupaBase from '../images/logos/supabase_logo_wordmark.png';
 
-
 type Partners = {
     name: string;
-    logo: any;
+    logo: StaticImageData;
     link: string;
-}
-
-
+};
 
 const partners: Partners[] = [
-    // partners name and logo goes here
     {
-      name: 'Dev Kenya',
-      logo: LogoDevKenya,
-      link: 'https://devkenya.com'
+        name: 'Dev Kenya',
+        logo: LogoDevKenya,
+        link: 'https://devkenya.com',
     },
     {
         name: 'ReactDevsKe',
         logo: LogoReactDevsKe,
-        link: 'https://reactdevske.org'
+        link: 'https://reactdevske.org',
     },
     {
         name: 'OSCA Nairobi',
         logo: LogoOscaNairobi,
-        link: 'https://github.com/OSCA-Nairobi'
+        link: 'https://github.com/OSCA-Nairobi',
     },
     {
-      name: 'Space Ya Tech',
-      logo: LogoSpaceYaTech,
-      link: 'https://spaceyatech.com/'
+        name: 'Space Ya Tech',
+        logo: LogoSpaceYaTech,
+        link: 'https://spaceyatech.com/',
     },
     {
-      name: 'Supabase',
-      logo: LogoSupaBase,
-      link: 'https://supabase.com/'
+        name: 'Supabase',
+        logo: LogoSupaBase,
+        link: 'https://supabase.com/',
     },
-  ]
+];
 
 const Partners = () => {
+    const addr = 'partnerships';
+    const tld = 'rendercon.org';
 
-    const addr = "partnerships";
-    const tld = "rendercon.org";
-
-    const handleEmailClick = (e : React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const handleEmailClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         e.preventDefault();
         window.location.href = `mailto:${addr}@${tld}`;
-    }
+    };
 
+    return (
+        <section id="partners" aria-label="Partners" className="py-20 sm:py-32">
+            <Container>
+                <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-slate-200 sm:text-5xl">
+                    Sponsors & Partners Hall of Fame
+                </h2>
 
-  return (
-    <section id="partners" aria-label="Partners" className="py-20 sm:py-32">
-      <Container>
-        <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-slate-200 sm:text-5xl">
-                Sponsors & Partners Hall of Fame
-        </h2>
-            
-        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
-          { partners.map((partner: any) => (
-            <div
-            key={partner.name}
-            className="flex items-center justify-center filter grayscale hover:grayscale-0 transition duration-300 ease-in-out"
-            >
-              <a href={partner.link} target="_blank" rel="noopener noreferrer">
-                <Image src={partner.logo} alt={partner.name} unoptimized height={150} />
-              </a>
-            </div>
-          ))}
-        </div>
-          <p className="text-slate-200 text-xl mt-20 font-mono text-center">
-              For more information on sponsorship and partnership, please reach out to us <a href="mailto: partnerships@rendercon.org" className='hover:underline text-[#eee712]'>here</a>
-          </p>
-      </Container>
-    </section>
-  )
-}
+                <div className="mx-auto flex items-center justify-center gap-24 mt-20 flex-row flex-wrap">
+                    {partners.map((partner) => (
+                        <div
+                            key={partner.name}
+                            className="flex items-center justify-center filter grayscale hover:grayscale-0 transition duration-300 ease-in-out"
+                        >
+                            <a href={partner.link} target="_blank" rel="noopener noreferrer">
+                                <Image src={partner.logo} alt={partner.name} unoptimized height={50} />
+                            </a>
+                        </div>
+                    ))}
+                </div>
+
+                <p className="text-slate-200 text-xl mt-20 font-mono text-center">
+                    For more information on sponsorship and partnership, please reach out to us{' '}
+                    <a href="#" onClick={handleEmailClick} className="hover:underline text-[#eee712]">
+                        here
+                    </a>
+                </p>
+            </Container>
+        </section>
+    );
+};
 
 export default Partners;
--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -1,70 +1,82 @@
-'use client';
+"use client";
 
-import Image, { StaticImageData } from 'next/image'
-
-import { Container } from './Container';
-import LogoDevKenya from '../images/logos/devkenya_logo.png';
-
-
+import Image, { StaticImageData } from "next/image";
+import { Container } from "./Container";
+import LogoLemonade from "../images/logos/lemonade.png";
+import LogoPayd from "../images/logos/payd.png";
+import LogoReactDevsKe from "../images/logos/ReactdevskeLogo.png";
 
 type Sponsors = {
-    name: string;
-    logo: string | StaticImageData;
-    link: string;
-}
-
-
+  name: string;
+  logo: string | StaticImageData;
+  link: string;
+};
 
 const sponsors: Sponsors[] = [
-    // sponsor name and logo goes here
-
-
-  ]
+  {
+    name: "ReactDevsKe", logo: LogoReactDevsKe, link: "https://reactdevske.org"},
+    { name: "Lemonade", logo: LogoLemonade, link: "https://mylemonade.io/" },
+  { name: "Payd", logo: LogoPayd, link: "https://paydexp.com/" },
+];
 
 const Sponsors = () => {
+  const addr = "sponsors";
+  const tld = "rendercon.org";
 
-    const addr = "sponsors";
-    const tld = "rendercon.org";
-
-    const handleEmailClick = (e : React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        e.preventDefault();
-        window.location.href = `mailto:${addr}@${tld}`;
-    }
-
+  const handleEmailClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    window.location.href = `mailto:${addr}@${tld}`;
+  };
 
   return (
     <section id="sponsors" aria-label="Sponsors" className="py-20 sm:py-32">
       <Container>
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-slate-200 sm:text-5xl">
-             Our Sponsors
+          Our Sponsors
         </h2>
-        <div className="mx-auto mt-20 max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
-          { sponsors.length > 0 ? 
-          sponsors.map((sponsor) => (
-            <>
-            <div
-              key={sponsor.name}
-              className="flex items-center justify-center"
-            >
-              <a href={sponsor.link} target="_blank" rel="noopener noreferrer">
-                <Image src={sponsor.logo} alt={sponsor.name} unoptimized height={150}/>
+        <div className="mx-auto flex items-center justify-center gap-24 mt-20 flex-col md:flex-row">
+          {sponsors.length > 0 ? (
+            sponsors.map((sponsor) => (
+              <div
+                key={sponsor.name}
+                className="flex  items-center justify-center"
+              >
+                <a
+                  href={sponsor.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src={sponsor.logo}
+                    alt={sponsor.name}
+                    unoptimized
+                    height={200}
+                    width={400}
+                  />
+                </a>
+              </div>
+            ))
+          ) : (
+            <p className="text-slate-300 text-xl font-mono text-center">
+              Leverage our platform to put your brand on the spotlight. Become a
+              part of RenderCon Kenya&apos;s mission to nurture innovation and
+              best practices within the React ecosystem.
+              <br /> Reach out for our sponsorship packages{" "}
+              <a
+                href="#"
+                onClick={handleEmailClick}
+                className="hover:underline text-[#eee712]"
+              >
+                here
               </a>
-            </div>
-
-              <p className="text-slate-400 text-xl font-mono text-center pt-5">
-                <br/> Reach out for our sponsorship packages <a href="mailto: sponsors@rendercon.org" className='hover:underline text-[#eee712]'>here</a>
-              </p>
-              </>
-            )) : (
-                <p className="text-slate-300 text-xl font-mono text-center">
-                    Leverage our platform to put your brand on the spotlight. Become a part of Rendercon Kenya&apos;s mission to nurture innovation and best practices within the React ecosystem.
-                    <br/> Reach out for our sponsorship packages <a href="mailto: sponsors@rendercon.org" className='hover:underline text-[#eee712]'>here</a>
-                </p>
-            )}
+            </p>
+          )}
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
 export default Sponsors;
