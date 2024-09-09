ad1d655399794014c5f7180f5ef877f4e28bfdf9
cindykandie <cindyjk6@gmail.com>
2024-09-09 23:31:05 +0300

    optimize the fonts and images to be more fitting on mobile screens

--- src/app/components/Technologies.tsx
+++ src/app/components/Technologies.tsx
@@ -5,14 +5,50 @@ type Props = {}
 
 const Technologies = (props: Props) => {
   return (
-    <div className='text-3xl text-slate-200'>
-        <Marquee  gradientWidth={700} speed={30} className='pb-5 overflow-hidden'>
-        
-        React, GraphQL, Nodejs, React Native, React Query, React Router, Nextjs, Redux, Remix, React Testing Library, Storybook, ChakraUI, Styled-components, Redux Toolkit, SWR, Framer Motion, Tailwindcss, Material UI, Cypress,
-        </Marquee>
-        <Marquee direction='right' gradientWidth={400} speed={30} className='pb-5 overflow-hidden'>
-        React, GraphQL, Nodejs, React Native, React Query, React Router, Nextjs, Redux, Remix, React Testing Library, Storybook, ChakraUI, Styled-components, Redux Toolkit, SWR, Framer Motion, Tailwindcss, Material UI, Cypress, 
-        </Marquee>
+    <div className='text-xl md:text-xl lg:text-3xl text-slate-200'>
+      <Marquee gradientWidth={700} speed={30} className='pb-5 overflow-hidden'>
+        <span className="text-[#61DAFB]">React</span>,&nbsp;
+        <span className="text-[#E10098]">GraphQL</span>,&nbsp;
+        <span className="text-[#68A063]">Node.js</span>,&nbsp;
+        <span className="text-[#61DAFB]">React Native</span>,&nbsp;
+        <span className="text-[#FF4154]">React Query</span>,&nbsp;
+        <span className="text-[#CA4245]">React Router</span>,&nbsp;
+        <span className="text-[#000000]">Next.js</span>,&nbsp;
+        <span className="text-[#764ABC]">Redux</span>,&nbsp;
+        <span className="text-[#3A69A8]">Remix</span>,&nbsp;
+        <span className="text-[#E33332]">React Testing Library</span>,&nbsp;
+        <span className="text-[#FF4785]">Storybook</span>,&nbsp;
+        <span className="text-[#319795]">ChakraUI</span>,&nbsp;
+        <span className="text-[#DB7093]">Styled-components</span>,&nbsp;
+        <span className="text-[#764ABC]">Redux Toolkit</span>,&nbsp;
+        <span className="text-[#CB0000]">SWR</span>,&nbsp;
+        <span className="text-[#0055FF]">Framer Motion</span>,&nbsp;
+        <span className="text-[#38B2AC]">TailwindCSS</span>,&nbsp;
+        <span className="text-[#00B0FF]">Material UI</span>,&nbsp;
+        <span className="text-[#00BF9A]">Cypress</span>,
+      </Marquee>
+      
+      <Marquee direction='right' gradientWidth={400} speed={30} className='pb-5 overflow-hidden'>
+        <span className="text-[#61DAFB]">React</span>,&nbsp;
+        <span className="text-[#E10098]">GraphQL</span>,&nbsp;
+        <span className="text-[#68A063]">Node.js</span>,&nbsp;
+        <span className="text-[#61DAFB]">React Native</span>,&nbsp;
+        <span className="text-[#FF4154]">React Query</span>,&nbsp;
+        <span className="text-[#CA4245]">React Router</span>,&nbsp;
+        <span className="text-[#000000]">Next.js</span>,&nbsp;
+        <span className="text-[#764ABC]">Redux</span>,&nbsp;
+        <span className="text-[#3A69A8]">Remix</span>,&nbsp;
+        <span className="text-[#E33332]">React Testing Library</span>,&nbsp;
+        <span className="text-[#FF4785]">Storybook</span>,&nbsp;
+        <span className="text-[#319795]">ChakraUI</span>,&nbsp;
+        <span className="text-[#DB7093]">Styled-components</span>,&nbsp;
+        <span className="text-[#764ABC]">Redux Toolkit</span>,&nbsp;
+        <span className="text-[#CB0000]">SWR</span>,&nbsp;
+        <span className="text-[#0055FF]">Framer Motion</span>,&nbsp;
+        <span className="text-[#38B2AC]">TailwindCSS</span>,&nbsp;
+        <span className="text-[#00B0FF]">Material UI</span>,&nbsp;
+        <span className="text-[#00BF9A]">Cypress</span>,
+      </Marquee>
     </div>
   )
 }
