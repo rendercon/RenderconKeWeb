874d898127964c51a50eace522022e76535e7bd3
orama254 <orama209@gmail.com>
2025-10-02 15:05:58 +0300

    chore: add vercel blob storage image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['sessionize.com'],
+        domains: ['sessionize.com', 'vo3gv2h0n9olcv4f.public.blob.vercel-storage.com'],
       },
 }
 
